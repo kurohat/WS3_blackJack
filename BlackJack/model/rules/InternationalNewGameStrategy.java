b57b84f7c450e39ea4097b41117a9fde541f2769
CHEAB <aa224iu@student.lnu.se>
2018-11-21 18:46:55 +0100

    DONE!!

--- BlackJack/model/rules/InternationalNewGameStrategy.java
+++ BlackJack/model/rules/InternationalNewGameStrategy.java
@@ -3,12 +3,10 @@ package BlackJack.model.rules;
 import BlackJack.model.Deck;
 import BlackJack.model.Dealer;
 import BlackJack.model.Player;
-import BlackJack.model.Card;  
 
 class InternationalNewGameStrategy implements INewGameStrategy {
 
   public boolean NewGame(Deck a_deck, Dealer a_dealer, Player a_player) {
-    Card c;
   
     a_dealer.DealCard(a_player, true);
 
